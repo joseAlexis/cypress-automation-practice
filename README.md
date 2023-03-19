491da37064b8e1bc775b9ed4dec9f5f12914c77b
Jose Bolanos <joseAlexis>
2023-03-19 11:58:16 +0100

    Small Updates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cypress-automation-practice
-Automation practice in Cypress
+Automation Practice in Cypress
+
+## Fearures
+1. Created in TS.
+2. Cypress Custom Commands.
+3. Tasks - Integrated with FakerJS.
+4. API request for preconditions.
+
--- cypress.config.ts
+++ cypress.config.ts
@@ -7,7 +7,7 @@ const baseUrl = 'https://automationexercise.com';
 export default defineConfig({
   watchForFileChanges: false,
   e2e: {
-    testIsolation: false,
+    testIsolation: true,
     baseUrl,
     setupNodeEvents(on, config) {
       on('task', {
